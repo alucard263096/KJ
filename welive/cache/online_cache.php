98b42d8bbd68d9745a057333a84b8a20faae729d
Steve Cai <steve.cai@morningstar.com>
2015-12-21 14:58:23 +0800

    上传welive代码

---
+++ welive/cache/online_cache.php
@@ -0,0 +1,35 @@
+<?php
+if(!defined('WELIVE')) die('File not found!');
+
+$welive_onlines  = array (
+  2 => 
+  array (
+    'groupname' => '销售咨询',
+    'groupename' => 'Sales Service',
+    'description' => '',
+    'descriptionen' => '',
+    'user' => 
+    array (
+      7 => 
+      array (
+        'username' => '旺得天下',
+        'type' => '1',
+        'isonline' => '0',
+        'isbusy' => '0',
+        'userfrontname' => '旺得天下',
+        'userfrontename' => 'wangwang',
+      ),
+      8 => 
+      array (
+        'username' => '阿勇',
+        'type' => '1',
+        'isonline' => '0',
+        'isbusy' => '0',
+        'userfrontname' => '阿勇',
+        'userfrontename' => 'ayong',
+      ),
+    ),
+  ),
+);
+
+?>
---
+++ welive/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername  = 'localhost';
+$dbname      = 'welive_1';
+$dbusername  = 'root';
+$dbpassword  = 'root';
+
+define('WELIVE', true);
+define('TABLE_PREFIX', 'welive_');
+define('COOKIE_KEY', 'p3kaeaKOwFB3');
+define('WEBSITE_KEY', '9v832vduU44p');
+define('BASEPATH', dirname(dirname(__FILE__)).'/');
+
+?>
